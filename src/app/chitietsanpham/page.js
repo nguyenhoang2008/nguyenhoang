1aed8bec8e7a7221716e95d49d9da2c7cb066730
nguyenhoang2008 <hoaho7834@gmail.com>
2025-05-17 15:40:18 +0700

    Add files via upload

---
+++ src/app/chitietsanpham/page.js
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import styles from "./chitietsanpham.module.css";
+import SimpleSlider from "../components/slider/slider";
+// import Slider from "react-slick/lib/slider";
+import Kmslider from "../components/slider/kmslider";
+
+
+export default function ChiTietSanPham() {
+    const htmlcontent = `<p>thong tin san pham</p>`;
+        return (
+            <div className={styles.ChiTietSanPham}>
+            <div className={styles.slide}> 
+                <div className={styles.image}>
+                  
+                        <SimpleSlider />
+                   
+                </div>
+                <div className={styles.thongtin} >
+                    <div className={styles.thongtinproduct} >
+                    <Kmslider />
+                    </div>
+                    <div className={styles.sell} >  
+                 
+                    <p>1000000đ</p> 
+                    <a>99999999999</a>
+                        </div>
+                    <div className={styles.sanpham}>
+                    <div className={styles.muangay} >   mua ngay  <br/>
+                        <span className={styles.giaohang}>giao hàng tận nơi hoặc tại cửa hàng</span></div> 
+                    </div>
+                </div>
+             </div>
+             <div className={styles.mota} >
+                <div dangerouslySetInnerHTML={{__html: `<p>
+                    Yoshiyuki Tomino và Sunries đã ra mắt TV series Mobile Suit Gundam với cụm từ Gundam (ガンダム) được dùng chỉ những mecha robot khổng lồ đặc biệt có khả năng chiến đấu cao, trang bị tốt và thiết kế bắt mắt, đẹp hơn so với các Mobile Suit sản xuất hàng loạt khác. 
+
+Gundam là gì
+
+Sự thành công của thương hiệu đã thúc đẩy Bandai tiếp tục sản xuất và cho ra mắt thêm nhiều series khác - đồng nghĩa với việc có thêm nhiều Gundam khác, song song với mỗi series sẽ là những bộ mô hình đồ chơi lắp ráp của các mẫu mecha xuất hiện trong phim, và chúng được gọi là Gunpla.
+Yoshiyuki Tomino và Sunries đã ra mắt TV series Mobile Suit Gundam với cụm từ Gundam (ガンダム) được dùng chỉ những mecha robot khổng lồ đặc biệt có khả năng chiến đấu cao, trang bị tốt và thiết kế bắt mắt, đẹp hơn so với các Mobile Suit sản xuất hàng loạt khác. 
+
+Gundam là gì
+
+Sự thành công của thương hiệu đã thúc đẩy Bandai tiếp tục sản xuất và cho ra mắt thêm nhiều series khác - đồng nghĩa với việc có thêm nhiều Gundam khác, song song với mỗi series sẽ là những bộ mô hình đồ chơi lắp ráp của các mẫu mecha xuất hiện trong phim, và chúng được gọi là Gunpla.
+Yoshiyuki Tomino và Sunries đã ra mắt TV series Mobile Suit Gundam với cụm từ Gundam (ガンダム) được dùng chỉ những mecha robot khổng lồ đặc biệt có khả năng chiến đấu cao, trang bị tốt và thiết kế bắt mắt, đẹp hơn so với các Mobile Suit sản xuất hàng loạt khác. 
+
+Gundam là gì
+
+Sự thành công của thương hiệu đã thúc đẩy Bandai tiếp tục sản xuất và cho ra mắt thêm nhiều series khác - đồng nghĩa với việc có thêm nhiều Gundam khác, song song với mỗi series sẽ là những bộ mô hình đồ chơi lắp ráp của các mẫu mecha xuất hiện trong phim, và chúng được gọi là Gunpla.</p>`}} ></div>
+Yoshiyuki Tomino và Sunries đã ra mắt TV series Mobile Suit Gundam với cụm từ Gundam (ガンダム) được dùng chỉ những mecha robot khổng lồ đặc biệt có khả năng chiến đấu cao, trang bị tốt và thiết kế bắt mắt, đẹp hơn so với các Mobile Suit sản xuất hàng loạt khác. 
+
+Gundam là gì
+
+Sự thành công của thương hiệu đã thúc đẩy Bandai tiếp tục sản xuất và cho ra mắt thêm nhiều series khác - đồng nghĩa với việc có thêm nhiều Gundam khác, song song với mỗi series sẽ là những bộ mô hình đồ chơi lắp ráp của các mẫu mecha xuất hiện trong phim, và chúng được gọi là Gunpla.
+Yoshiyuki Tomino và Sunries đã ra mắt TV series Mobile Suit Gundam với cụm từ Gundam (ガンダム) được dùng chỉ những mecha robot khổng lồ đặc biệt có khả năng chiến đấu cao, trang bị tốt và thiết kế bắt mắt, đẹp hơn so với các Mobile Suit sản xuất hàng loạt khác. 
+
+Gundam là gì
+
+Sự thành công của thương hiệu đã thúc đẩy Bandai tiếp tục sản xuất và cho ra mắt thêm nhiều series khác - đồng nghĩa với việc có thêm nhiều Gundam khác, song song với mỗi series sẽ là những bộ mô hình đồ chơi lắp ráp của các mẫu mecha xuất hiện trong phim, và chúng được gọi là Gunpla.
+Yoshiyuki Tomino và Sunries đã ra mắt TV series Mobile Suit Gundam với cụm từ Gundam (ガンダム) được dùng chỉ những mecha robot khổng lồ đặc biệt có khả năng chiến đấu cao, trang bị tốt và thiết kế bắt mắt, đẹp hơn so với các Mobile Suit sản xuất hàng loạt khác. 
+
+Gundam là gì
+
+Sự thành công của thương hiệu đã thúc đẩy Bandai tiếp tục sản xuất và cho ra mắt thêm nhiều series khác - đồng nghĩa với việc có thêm nhiều Gundam khác, song song với mỗi series sẽ là những bộ mô hình đồ chơi lắp ráp của các mẫu mecha xuất hiện trong phim, và chúng được gọi là Gunpla.
+Yoshiyuki Tomino và Sunries đã ra mắt TV series Mobile Suit Gundam với cụm từ Gundam (ガンダム) được dùng chỉ những mecha robot khổng lồ đặc biệt có khả năng chiến đấu cao, trang bị tốt và thiết kế bắt mắt, đẹp hơn so với các Mobile Suit sản xuất hàng loạt khác. 
+
+Gundam là gì
+
+Sự thành công của thương hiệu đã thúc đẩy Bandai tiếp tục sản xuất và cho ra mắt thêm nhiều series khác - đồng nghĩa với việc có thêm nhiều Gundam khác, song song với mỗi series sẽ là những bộ mô hình đồ chơi lắp ráp của các mẫu mecha xuất hiện trong phim, và chúng được gọi là Gunpla.
+Yoshiyuki Tomino và Sunries đã ra mắt TV series Mobile Suit Gundam với cụm từ Gundam (ガンダム) được dùng chỉ những mecha robot khổng lồ đặc biệt có khả năng chiến đấu cao, trang bị tốt và thiết kế bắt mắt, đẹp hơn so với các Mobile Suit sản xuất hàng loạt khác. 
+
+Gundam là gì
+
+Sự thành công của thương hiệu đã thúc đẩy Bandai tiếp tục sản xuất và cho ra mắt thêm nhiều series khác - đồng nghĩa với việc có thêm nhiều Gundam khác, song song với mỗi series sẽ là những bộ mô hình đồ chơi lắp ráp của các mẫu mecha xuất hiện trong phim, và chúng được gọi là Gunpla.
+             </div>
+            </div>
+        );
+    }
---
+++ src/app/mucluc/page.js
@@ -0,0 +1,12 @@
+import styles from "./mucluc.module.css";
+
+export default  function Mucluc(){
+    return (
+
+  <h1>Mục Lục Gundam</h1>
+  <div id="gundamCatalog"></div>
+
+  <script src="script.js"></script>
+
+    )
+}
